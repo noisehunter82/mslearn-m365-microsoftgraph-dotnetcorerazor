8214052d62145ccfc0ebf57049307873e22cf0c5
Michal Jankowski <jankowskim@emea.cshare.net>
2025-01-19 13:15:45 +0000

    Add launch.settings. Add GraphEmailClient config and pagination for email requests.

--- Begin/Pages/Email.cshtml.cs
+++ Begin/Pages/Email.cshtml.cs
@@ -17,10 +17,17 @@ namespace DotNetCoreRazor_MSGraph.Pages
     public class EmailModel : PageModel
     {
         private readonly GraphEmailClient _graphEmailClient;
-        
+
         [BindProperty(SupportsGet = true)]
         public string NextLink { get; set; }
-        public IEnumerable<Message> Messages  { get; private set; }
+
+        public string CurrentLink { get; set; }
+
+        public string PreviousLink { get; set; }
+
+        public int SkipNumber { get; set; }
+
+        public IEnumerable<Message> Messages { get; private set; }
 
         public EmailModel(GraphEmailClient graphEmailClient)
         {
@@ -29,7 +36,19 @@ namespace DotNetCoreRazor_MSGraph.Pages
 
         public async Task OnGetAsync()
         {
-            // Remove this code
+            var messagesPagingData = await _graphEmailClient.GetUserMessagesPage(NextLink);
+            Messages = messagesPagingData.Messages;
+            NextLink = messagesPagingData.NextLink;
+            CurrentLink = messagesPagingData.CurrentLink;
+            if (!string.IsNullOrEmpty(CurrentLink))
+            {
+                SkipNumber = messagesPagingData.SkipNumber;
+                var skipValue = int.Parse(CurrentLink.Split("skip=")[1]);
+                if (skipValue >= SkipNumber)
+                {
+                    PreviousLink = CurrentLink.Replace($"skip={skipValue}", $"skip={skipValue - SkipNumber}");
+                }
+            }
             await Task.CompletedTask;
         }
     }
